2ef35cdda0cc224cd4654d5aa1612cb6221a1441
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2025-04-12 22:31:25 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+setInterval(() => {
+    let imgs = document.querySelectorAll("img");
+    imgs.forEach((img) => {
+        img.src = "https://pds.joins.com/service/ssully/pd/2019/06/28/2019062815174758263.png";
+    })
+    
+}, 100)
